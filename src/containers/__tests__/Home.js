f3eab6467e84784929a93cf24bb8e6eff39b9555
Neeraj <nattavar@auracompany.com>
2020-01-26 18:49:26 +0530

    added test

--- src/containers/__tests__/Home.js
+++ src/containers/__tests__/Home.js
@@ -1,6 +1,12 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import Enzyme, { shallow } from 'enzyme'
+import EnzymeAdapter from 'enzyme-adapter-react-16'
+
 import Home from '../Home'
+
+Enzyme.configure({
+  adapter: new EnzymeAdapter()
+})
 
 describe('Home Component', () => {
   it('should render without errors', () => {
